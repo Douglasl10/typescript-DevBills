ea79112d73b9672341fa1fb49dc832800859fe0b
Douglas <ftsgmr@gmail.com>
2025-08-03 08:23:11 -0300

    up

--- src/routes/caterory.routes.ts
+++ src/routes/caterory.routes.ts
@@ -3,8 +3,8 @@ import { getCategories } from "../controllers/category.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 
 const categoryRoutes = async (fastify: FastifyInstance): Promise<void> => {
-	fastify.get("/", getCategories);
 	fastify.addHook("preHandler", authMiddleware);
+	fastify.get("/", getCategories);
 };
 
 export default categoryRoutes;
